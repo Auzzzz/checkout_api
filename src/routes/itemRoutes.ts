bb4a35f80f6a44d6cce12899935fa2f3daa15e81
Auzzzz <chris@downunderaussie.com>
2023-12-20 20:57:30 +1100

    JWT checker w/ fusion & local check

--- src/routes/itemRoutes.ts
+++ src/routes/itemRoutes.ts
@@ -2,13 +2,15 @@ import express from "express";
 import { validate } from "../validation";
 import { createVenueSchema, IdOnlySchema } from "../models/generalModel";
 import itemsController from "../controllers/itemsController";
+import {isAuthed, isAuthedFusion} from "../services/auth";
 const router = express.Router();
+
 
 router.get("/", validate(IdOnlySchema), itemsController.getItem);
 router.post("/", validate(createVenueSchema), itemsController.postItem);
 router.delete("/", validate(IdOnlySchema), itemsController.deleteItem);
 router.put("/", validate(createVenueSchema), itemsController.updateItem);
 
-router.get("/all", itemsController.getItems);
+router.get("/all", isAuthedFusion, itemsController.getItems);
 
 export default router;
